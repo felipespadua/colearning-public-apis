bd1df753f5b4f5c1d5771fcbe65e3ac314dd423c
Felipe Padua <felipepadua@MacBook-Pro-de-Felipe.local>
2021-08-19 22:05:12 -0300

    add resolution

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/assets", async (req, res) => {
-    const { limit = 10, offset = 0 } = req.query;
+    const { limit = 10, offset = 0, ids, search } = req.query;
     try{
-        
         const response = await axios.get('http://api.coincap.io/v2/assets',
             {
                 params: {
                     limit,
-                    offset
+                    offset,
+                    ids,
+                    search
                 }
             })
             .then(response => response.data)
@@ -21,6 +22,20 @@ app.get("/assets", async (req, res) => {
         res.status(200).json(response)
     }catch(error){
         console.log(error)
+        res.status(500).json("Internal server error")
+    }
+})
+
+app.get("/assets/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        const response = await axios.get(`http://api.coincap.io/v2/assets/${id}`)
+            .then(response => response.data)
+
+        res.status(200).json(response)
+    }catch(error){
+        console.log(error)
+        res.status(500).json("Internal server error")
     }
 })
 
